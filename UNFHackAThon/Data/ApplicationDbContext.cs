4fe385997a80ad42039359b3b380bc17a17d44db
Michael Zulfekar <n00929403@ospreys.unf.edu>
2020-01-26 01:14:05 -0500

    Added Competition Model

--- UNFHackAThon/Data/ApplicationDbContext.cs
+++ UNFHackAThon/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UNFHackAThon.Models;
 
 namespace UNFHackAThon.Data
 {
@@ -12,5 +13,7 @@ namespace UNFHackAThon.Data
             : base(options)
         {
         }
+
+        public DbSet<Competition> Competition { get; set; }
     }
 }
---
+++ UNFHackAThon/Models/Competition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UNFHackAThon.Models
+{
+    public class Competition
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
