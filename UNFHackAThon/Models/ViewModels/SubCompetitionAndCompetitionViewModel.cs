76e34b07112ffb7058835998e2296d5ed9aa5042
Michael Zulfekar <n00929403@ospreys.unf.edu>
2020-01-26 15:48:26 -0500

    SubCompetition and Competition View Model

---
+++ UNFHackAThon/Models/ViewModels/SubCompetitionAndCompetitionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UNFHackAThon.Models.ViewModels
+{
+    public class SubCompetitionAndCompetitionViewModel
+    {
+        public IEnumerable<Competition> CompetitionList { get; set; }
+        public SubCompetition SubCompetition { get; set; }
+        public List<string> SubCompetitionList { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
