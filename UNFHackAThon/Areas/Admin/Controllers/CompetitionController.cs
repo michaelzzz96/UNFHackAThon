4cda9e9be9e2f757540eaf9b754a92d71630996b
Michael Zulfekar <n00929403@ospreys.unf.edu>
2020-01-26 01:39:10 -0500

    Add Competition Controller and Action Result

---
+++ UNFHackAThon/Areas/Admin/Controllers/CompetitionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UNFHackAThon.Data;
+
+namespace UNFHackAThon.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompetitionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CompetitionController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET 
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _db.Competition.ToListAsync());
+        }
+    }
+}
