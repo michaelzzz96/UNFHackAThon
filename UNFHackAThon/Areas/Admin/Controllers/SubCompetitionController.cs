d7c4cadebc3376d18b3e6c77113bf2e1a95d33e7
Michael Zulfekar <n00929403@ospreys.unf.edu>
2020-01-26 17:10:09 -0500

    SubCompetion Create POST

--- UNFHackAThon/Areas/Admin/Controllers/SubCompetitionController.cs
+++ UNFHackAThon/Areas/Admin/Controllers/SubCompetitionController.cs
@@ -39,6 +39,34 @@ namespace UNFHackAThon.Areas.Admin.Controllers
             return View(model);
         }
 
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCompetitionAndCompetitionViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var doesSubCompetitionExists = _db.SubCompetition.Include(s => s.Competition).Where(s => s.Name == model.SubCompetition.Name && s.Competition.Id == model.SubCompetition.CompetitionId);
+
+                    if(doesSubCompetitionExists.Count() > 0)
+                    {
+                        //Error 
+                    }
+                else
+                {
+                    _db.SubCompetition.Add(model.SubCompetition);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            SubCompetitionAndCompetitionViewModel modelVM = new SubCompetitionAndCompetitionViewModel()
+            {
+                CompetitionList = await _db.Competition.ToListAsync(),
+                SubCompetition = model.SubCompetition,
+                SubCompetitionList = await _db.SubCompetition.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync()
+            };
+            return View(modelVM);
+        }
 
     }
 }
