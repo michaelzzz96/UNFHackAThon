b1e70ef14b23b8e39e08e68e8b436f25633c290a
Michael Zulfekar <n00929403@ospreys.unf.edu>
2020-01-27 11:49:40 -0500

    Step 1 of Home View Designing

--- UNFHackAThon/Areas/Participants/Controllers/HomeController.cs
+++ UNFHackAThon/Areas/Participants/Controllers/HomeController.cs
@@ -4,16 +4,30 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UNFHackAThon.Data;
 using UNFHackAThon.Models;
+using UNFHackAThon.Models.ViewModels;
 
 namespace UNFHackAThon.Controllers
 {
     [Area("Participants")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                CompetitionItem = await _db.CompetitionItem.Include(m => m.Competition).Include(m => m.SubCompetition).ToListAsync(),
+                Competition = await _db.Competition.ToListAsync(),
+            };
+            return View(IndexVM);
         }
 
         public IActionResult Privacy()
---
+++ UNFHackAThon/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UNFHackAThon.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<CompetitionItem> CompetitionItem { get; set; }
+        public IEnumerable<Competition> Competition { get; set; }
+    }
+}
